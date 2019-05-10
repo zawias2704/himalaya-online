b92ffbb14d9b93d354833e214c4fd24610e314ed
zawias <zawias@zawias.pro>
2019-05-10 13:17:33 +0200

    jsonToHtml - fix spacing, prettify output

--- src/containers/Parser/jsonToHtml.js
+++ src/containers/Parser/jsonToHtml.js
@@ -1,10 +1,26 @@
 import { stringify } from 'himalaya';
+import prettifyHtml from 'prettify-html';
+
+const transformJsonIntoHtml = (input) => {
+  let result = input;
+
+  // remove "export default " if any
+  result = result.replace('export default ', '');
+
+  // use himalaya to transform json into html
+  result = stringify(JSON.parse(result));
+
+  // prettify the html code
+  result = prettifyHtml(result);
+
+  return result;
+};
 
 const jsonToHtml = (jsonString) => {
   let result;
 
   try {
-    result = stringify(JSON.parse(jsonString))
+    result = transformJsonIntoHtml(jsonString);
   } catch (e) {
     result = 'Error! Root element must be an array.\n\nMessage:\n ' + e;
   }
