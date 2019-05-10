2dc8f63b8c3d7a3b11d391310b0e08c8df1b1a59
zawias <zawias@zawias.pro>
2019-05-10 13:01:46 +0200

    htmlToJson now uses proper spacing

--- src/containers/Parser/htmlToJson.js
+++ src/containers/Parser/htmlToJson.js
@@ -1,7 +1,24 @@
 import { parse } from 'himalaya';
 
 const htmlToJson = (htmlString) => {
-  return JSON.stringify(parse(htmlString), null, 2);
+  let result = htmlString;
+
+  // remove all newlines
+  result = result.replace(/(\r\n|\n|\r)/gm, '');
+
+  // remove 2+ consecutive whitespaces
+  result = result.replace(/\s+/g," ");
+
+  // remove spacing between tags
+  result = result.replace(/> </g,"><");
+
+  // parse with himalaya
+  result = parse(result);
+
+  // stringify (2 spaces align)
+  result = JSON.stringify(result, null, 2);
+
+  return result;
 };
 
 export default htmlToJson;
